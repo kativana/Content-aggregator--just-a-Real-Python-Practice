a3262bec31f5e982ecf83b7e4367ad3300c5098c
kativana <102433058+kativana@users.noreply.github.com>
2022-06-14 15:38:07 +0200

    Add files via upload

---
+++ podcasts/management/commands/startjobs.py
@@ -0,0 +1,67 @@
+
+
+from django.core.management.base import BaseCommand
+
+
+import feedparser
+
+from dateutil import parser
+
+
+from podcasts.models import Episode
+
+
+def save_new_episodes(feed):
+
+
+    podcast_title = feed.channel.title
+
+    podcast_image = feed.channel.image["href"]
+
+
+    for item in feed.entries:
+
+        if not Episode.objects.filter(guid=item.guid).exists():
+
+            episode = Episode(
+
+                title=item.title,
+
+                description=item.description,
+
+                pub_date=parser.parse(item.published),
+
+                link=item.link,
+
+                image=podcast_image,
+
+                podcast_name=podcast_title,
+
+                guid=item.guid,
+
+            )
+
+            episode.save()
+
+
+def fetch_realpython_episodes():
+
+    _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+
+    save_new_episodes(_feed)
+
+
+def fetch_talkpython_episodes():
+
+    _feed = feedparser.parse("https://talkpython.fm/episodes/rss")
+
+    save_new_episodes(_feed)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        fetch_realpython_episodes()
+
+        fetch_talkpython_episodes()
